a53fbc0f610b09d3c6fa3fca3adeae60829e77a3
ElMora96 <francescomoraglio6@gmail.com>
2021-06-08 11:15:34 +0200

    Main 4 Monticello

--- main_monticello.py
+++ main_monticello.py
@@ -27,8 +27,7 @@ plt.rc('lines', markersize = 10) #18 for cv plots
 # Palette of colors to be used for plotting the results
 colors = [(230, 25, 75),
 		(60, 180, 75),
-		(255, 225, 25),
-		
+		(255, 225, 25),		
 		(0, 130, 200),
 		(245, 130, 48),
 		(145, 30, 180),
@@ -55,34 +54,24 @@ for color in colors:
 		color_rgb.append(float(value)/255) 
 	colors_rgb.append(tuple(color_rgb))
 
-
-###############################################################################
-
 ### Parameters setup
+dt_aggr = 60 #Fixed
 
+##Choose RSA/No RSA Scenario
+scenario = input("Select scenario of interest. 1 to exclude the RSA; 2 to include the RSA \n >>>")
+scenario = int(scenario)
+if scenario not in [1, 2]:
+	raise ValueError("Please select proper scenario (1 or 2)")
 # Contractual power (MAX) (kW)
-power_max = 140.5#105.5 - 140.5
-
-# Time-step used to aggregated the results (min): 1 | 5 | 10 | 15 | 10 | 30 | 45 | 60 
-# FIXED PARAM : 60 minutes
-dt_aggr = 60 
-
-
-## Type of simulation
-# The type of simulation is chosen (fixed size or paramtric analysis) both for the pv
-# and the battery and the ranges for the sizes are specified as well. This is done using the method
-# simulation_setup from parameters_input.py (inp) that returns a dictionary and a list.
-
+power_max = 105.5 if scenario == 1 else 140.5
+# Power yearly expense
+yearly_expense = 19356
+#Producer (investor) benefit share
+beta_prod = 1 if scenario == 1 else 0.75
 # Photovoltaic (PV)
 pv_setup, pv_size_range = inp.simulation_setup('PV')
-
 # Battery 
 battery_setup, battery_size_range = inp.simulation_setup('battery')
-
-
-message = '\nNo consumption simulation is performed. Data are provided by User.'
-print(message) 
-
 
 # Simulation setup (PV)
 tab = []
@@ -95,7 +84,6 @@ print(message)
 print(tabulate(tab, headers=['Parameter', 'Value']))
 
 # Simulation setup (battery)
-
 tab = []
 for param in battery_setup:
 	row = [param, battery_setup[param]]
@@ -105,12 +93,7 @@ message = '\nSimulation setup (battery)'
 print(message) 
 print(tabulate(tab, headers=['Parameter', 'Value']))
 
-
-## Building seasons, months and week dictionaries 
-# This is done in order to explore different seasons/months and day-types (week-day/weekend-day)
-# during the simulations (id numbers and nicknames are needed to find the various files that are 
-# to be uploaded and then find the correct positions in arrays for each data)
- 
+#Misc Runtime Dictionaries 
 seasons = {
 	'winter': (0, 'w'),
 	'spring': (1, 'ap'),
@@ -123,14 +106,6 @@ days = {
 	'weekend-day': (1, 'we')
 	}
 
-# While the routine that evaluates the load profiles works with typical profiles for each season, 
-# the optimization procedure works with tyipcal profiles for each month.  
-# A reference year is considered, in which the first day (01/01) is a monday. 
-# Therefore, conventionally considering that winter lasts from january to march 
-# spring from april to june, summer from july to september and autumn from october
-# to december, each month has got the following number of weekdays and weekend days.
-
-# Creating a dictionary for the months
 months = {
 	'january': {'id': (0, 'jan'), 'season': 'winter', 'days_distr': {'week-day': 23, 'weekend-day': 8}},
 	'february': {'id': (1, 'feb'), 'season': 'winter', 'days_distr': {'week-day': 20, 'weekend-day': 8}},
@@ -144,7 +119,7 @@ months = {
 	'october': {'id': (9, 'oct'), 'season': 'autumn', 'days_distr': {'week-day': 23, 'weekend-day': 8}},
 	'november': {'id': (10, 'nov'), 'season': 'autumn', 'days_distr': {'week-day': 22, 'weekend-day': 8}},
 	'december': {'id': (11, 'dec'), 'season': 'autumn', 'days_distr': {'week-day': 21, 'weekend-day': 10}},
-	}
+	}	
 
 # The days distribution in the seasons can be evaluated as well
 days_distr = {}
@@ -162,8 +137,7 @@ for month in months:
 n_months = len(months)
 n_seasons = len(seasons)
 n_days = len(days)
-
-# Storing all these dictionaries in a new dict that is passed to the various methods
+	
 auxiliary_dict = {
 	'seasons': seasons,
 	'n_seasons': n_seasons,
@@ -174,25 +148,13 @@ auxiliary_dict = {
 	'days_distr': days_distr,
 	}
 
-# Here the core of the simulation starts
 tic()
-
-
-## Time discretization
-# Time is discretized in steps of one hour (according to the definition of shared energy in the Decree Law 162/2019 - "Mille proroghe")
-
-# Total time of simulation (h) - for each typical day
+ 
+#Time variables
 time = 24
-
-# Timestep for the simulation (h) (the keyboard input dt_aggr is given in minutes)
-#Fixed timestep - 1h
 dt = 1 #dt_aggr/60
-
-# Vector of time, from 00:00 to 23:59, i.e. 24 h
 time_sim = np.arange(0, time, dt)
 time_length = np.size(time_sim)
-
-# Storing all the elements needed for the time-discretization in a dictionary that is passed to the various methods
 time_dict = {
 	'time': time,
 	'dt': dt,
@@ -200,18 +162,8 @@ time_dict = {
 	'time_length': time_length,
 	}
 
-
-
-### Input data
-
 # Maximum power from the grid (total) (kW)
 grid_power_max = power_max
-
-
-## Battery specification
-# The battery specifications are stored in a file that can be read using the method read_param
-# from the module datareader.py, that will return a dictionary
-
 battery_specs = datareader.read_param('battery_specs.csv', ';', 'Input')
 
 # Storing the information about the various technologies considered in a dictionary
@@ -221,42 +173,25 @@ technologies_dict = {
 	'battery_specs': battery_specs,
 	}
 
-
-## Unit production from the photovoltaic installation (kWh/h/kWp)
-# The unit production during each hour (kWh/h/kWp) from the photovoltaic installation can 
-# be read using the method read_general from the module datareader.py, that returns a 2d-array
-# containing the time vector on the first columns and the unit production during each hour in each
-# month in the other columns
-
-#Little ugly
-import pvgis_to_csv #runs PVGIS Data transformation
+#PV
 data_pv = datareader.read_general('pv_production_unit.csv', ';', 'Input')
-
 time_pv = data_pv[:, 0]
 pv_production_unit = data_pv[:, 1:]
-
-# Interpolating the unit pv production, if it has a different time resolution
-if (time_pv[-1] - time_pv[0])/(np.size(time_pv) - 1) != dt:
-	
-	f_pv = interp1d(time_pv, pv_production_unit, kind = 'linear', axis = 0, fill_value = 'extrapolate')
-	pv_production_unit = f_pv(time_sim)
-
-
-## Consumption from the aggregate of PODs
 
 # Checking if there is already a file where the load profiles for this configuration have been stored
 dirname = Path('Input')
 subdirname = Path('Files')
 subsubdirname = Path('Profiles')
+lastdirname = Path('sim_no_rsa') if scenario == 1 else Path('sim_rsa')
 
 # If the files exist, program reads it
 try:
 	#data_wd = datareader.read_general('consumption_profiles_month_wd.csv', ';', '/'.join((dirname, subdirname, subsubdirname)))
-	data_wd = pd.read_csv(basepath / dirname / subdirname / subsubdirname / 'consumption_profiles_month_wd.csv',
+	data_wd = pd.read_csv(basepath / dirname / subdirname / subsubdirname / lastdirname / 'consumption_profiles_month_wd.csv',
 						  sep = ";",
 						  decimal = ",",
 						  ).dropna().values
-	data_we = pd.read_csv(basepath / dirname / subdirname / subsubdirname / 'consumption_profiles_month_we.csv',
+	data_we = pd.read_csv(basepath / dirname / subdirname / subsubdirname / lastdirname / 'consumption_profiles_month_we.csv',
 						  sep = ";",
 						  decimal = ",",
 						  ).dropna().values
@@ -264,19 +199,16 @@ try:
 	consumption_month_we = data_we[:, 1:]
 	consumption_month_day = np.stack((consumption_month_wd, consumption_month_we), axis = 2)
 except:
-	raise RuntimeError("Must provide load profiles in proper format.") # load_profiler_flag = 1
+	raise RuntimeError("Must provide load profiles in proper format.") 
 
-### Energy shared from the aggregate of PODs during one year, for all possible configurations
-
-# Total number of configurations to be analysed
+	# Total number of configurations to be analysed
 n_pv_sizes = len(pv_size_range)
 n_battery_sizes = len(battery_size_range)
 n_configurations = n_pv_sizes*n_battery_sizes
 
-# Creating a flag that is used to assess if a fixed analysis is performed on both technologies
-# In this case detailed results are stored in .csv files and figures
 fixed_analysis_flag = 0
-if n_configurations == 1: fixed_analysis_flag = 1
+if n_configurations == 1: 
+	fixed_analysis_flag = 1
 
 # Initializing a list where to store some relevant results to be printed as a tabulate
 tab_results = []
@@ -294,8 +226,7 @@ if n_configurations  != 1:
 	# Self-consumption index in a year for each configuration
 	sci_configurations = np.zeros((n_pv_sizes, n_battery_sizes))
 
-
-# A message is printed to let the user know how the simulation is proceeding (every 25% of progress)
+	# A message is printed to let the user know how the simulation is proceeding (every 25% of progress)
 perc_configurations = 25
 count_configurations = 0
 
@@ -331,14 +262,8 @@ for pv_size in pv_size_range:
 
 		
 		## Calling the method that computes the energy shared during one year
-		# For each typical day (two for each month, i.e. 24 typical days) the operation of the battery
-		# is optimised using a MILP procedure in order to minimize the exchanges with the grid
-		# The method that evaluates the shared energy returns a series of arrays having the same size
-		# as the number of months, where values for the energy (shared, consumed, produced, etc.) are stored
 
 		results = shared_energy_evaluator(time_dict, input_powers_dict, technologies_dict, auxiliary_dict, fixed_analysis_flag)
-
-		# Storing the results
 
 		# Status of the optimisation during the current typical day
 		optimisation_status = results['optimisation_status']
@@ -397,14 +322,26 @@ for pv_size in pv_size_range:
 		# Self-consumption index in a year (%)
 		self_cons_ind_year = shared_energy_year/(pv_production_year)*100
 
+		#Compute KPI
+		I0 = initial_investment(pv_size, battery_size) #initial investment
+		cflows, energy_sales, contrib, contrib_prod = cash_flows(grid_feed_year/1000,
+														     	shared_energy_year/1000,
+															    pv_size, 
+																beta = beta_prod,
+																n_years = 20,
+																inf_rate= 0.01
+																)
+		#Compute NPV and IRR
+		cer_npv = NPV(cflows, 0.02, I0)
+		cer_irr = IRR(cflows, I0)
 
-		## Post-processing of the results 
-		# If different configuration are considered, some quantities (iss, isc, esh) are stored for each configuration
-		# in order to be plotted outside of the loops (that iterate among configurations). The tabulate is instead built 
-		# inside the loops to avoid creating too many variable for storing the results.
-		# If only one configuration is considered the tabulate can be created outside the loop without any change on the final result
+		#Compute PCR
+		cer_pcr = PCR(yearly_expense, contrib_prod, energy_sales, I0, power_max)
 
-		# In case at least one technology is subject to a parametric analysis, only yearly quantities are shown
+		#Payback time
+		cer_pbt = PBT(I0, cflows)
+
+				# In case at least one technology is subject to a parametric analysis, only yearly quantities are shown
 		if fixed_analysis_flag != 1:
 
 			# Storing the yearly values of the self-sufficiency and self-consumption indices and of the shared energy 
@@ -415,35 +352,23 @@ for pv_size in pv_size_range:
 			esh_configurations[pv_index, battery_index] = np.sum(shared_energy)
 
 			# Storing the results in a tabularr form
-
-			# List of days in which the optimisation did not work or was infeasible
-			opt_did_not_work_list = []
-			# opt_unnecessary_list = []
-			opt_infeasible_list = []
-
-			# Optimisation status
-			for month in months:
-				month_nickname = months[month]['id'][1]
-
-				for day in days:
-					day_nickname = days[day][1]
-
-					opt_status = optimisation_status[month][day].lower().strip('\'",._- ').replace('.','').replace(' ','_')
-					if opt_status == 'opt_did_not_work': opt_did_not_work_list.append('{}, {}'.format(month_nickname.capitalize(), day_nickname))
-					# elif opt_status == 'opt_unnecessary': opt_unnecessary_list.append('{}, {}'.format(month_nickname.capitalize(), day_nickname))
-					elif opt_status == 'infeasible': opt_infeasible_list.append('{}, {}'.format(month_nickname.capitalize(), day_nickname))
-
-			# Energy values in a year (kWh/year)
-			row = [pv_size, battery_size, '\n'.join(opt_did_not_work_list), '\n'.join(opt_infeasible_list), \
-				'{0:.2f}'.format(self_suff_ind_year), \
-				'{0:.2f}'.format(self_cons_ind_year), \
-				'{0:.1f}'.format(np.sum(shared_energy)), \
-				'{0:.1f}'.format(np.sum(pv_production_energy)), \
-				'{0:.1f}'.format(np.sum(consumption_energy)), \
-				'{0:.1f}'.format(np.sum(grid_feed_energy)), \
-				'{0:.1f}'.format(np.sum(grid_purchase_energy)), \
-				'{0:.1f}'.format(np.sum(battery_charge_energy)), \
-				'{0:.1f}'.format(np.sum(battery_discharge_energy)), \
+			row = [pv_size, battery_size,
+				'{0:.2f}'.format(self_suff_ind_year), 
+				'{0:.2f}'.format(self_cons_ind_year), 
+				'{0:.1f}'.format(np.sum(shared_energy)), 
+				'{0:.1f}'.format(np.sum(pv_production_energy)), 
+				'{0:.1f}'.format(np.sum(consumption_energy)), 
+				'{0:.1f}'.format(np.sum(grid_feed_energy)), 
+				'{0:.1f}'.format(np.sum(grid_purchase_energy)), 
+				'{0:.1f}'.format(np.sum(battery_charge_energy)), 
+				'{0:.1f}'.format(np.sum(battery_discharge_energy)), 
+				'{0:.0f}'.format(cer_npv), #NPV
+				'{0:.2f}'.format(cer_irr * 100), #IRR
+				'{0:.1f}'.format(energy_sales.sum()), #Energy Sales 
+				'{0:.1f}'.format(contrib_prod.sum()), #Producer Contributions
+				'{0:.1f}'.format(contrib.sum() - contrib_prod.sum()), #Non-producer Contributions
+				'{0:.1f}'.format(cer_pcr), #PCR
+				'{}'.format(cer_pbt) #PBT
 				]
 
 			tab_results.append(row)
@@ -457,19 +382,14 @@ for pv_size in pv_size_range:
 # Informing the user about the total time needed to evaluate the configurations
 print('\n{0:d} configuration(s) evaluated in {1:.3f} s.'.format(n_configurations, toc()))
 
-
-
-### Post-processing of the results (tabulates, .csv files and figures)
-
-
-## Completing the tabulate that is going to be printed
-
 # Header to be used in the tabulate in case of parametric analysis at least for one of the two technologies
 if fixed_analysis_flag != 1:
-	headers = ['PV size \n(kW)', 'Battery size \n(kWh)', 'Opt. did not work in\n*values fixed', 'Opt. infeasible in\n*minor issue', \
+	headers = ['PV size \n(kW)', 'Battery size \n(kWh)', 
 				'SSI \n(%)', 'SCI \n(%)', 'Shared energy \n(kWh/year)', 'PV production \n(kWh/year)', 'Consumption \n(kWh/year)', \
-				'Grid feed \n(kWh)', 'Grid purchase \n(kWh)', 'Battery charge \n(kWh)', 'Battery discharge \n(kWh)']
-
+				'Grid feed \n(kWh)', 'Grid purchase \n(kWh)', 'Battery charge \n(kWh)', 'Battery discharge \n(kWh)',
+				'NPV (€)', 'IRR (%)', 'Energy Sales Next 20 Years (€)', 'Producer Contributions Next 20 Years (€)', 'Non-Producer Contributions Next 20 Years (€)',
+				'PCR Yearly (%)', 'PBT (Years)'
+				]
 # In case of fixed size simulation for both the PV and the battery the tabulate has still to be built
 # Morover, detailed results are provided (monthly)
 else:
@@ -517,7 +437,6 @@ else:
 				'{0:.1f}'.format(battery_discharge_year), \
 				])
 
-
 # Printing the tabulate
 message = '\nOptimisation status and results (self-sufficiency and self-consumption indices, shared energy, etc.)\n'
 print(message) 
@@ -528,18 +447,15 @@ print(tabulate(tab_results, headers = headers))
 
 # Creating an /Output folder, if not already existing
 dirname = 'Output'
-
 try: Path.mkdir(basepath / dirname)
 except Exception: pass
 
 # Creating an /Output/Files folder, if not already existing
 subdirname = 'Files'
-
 try: Path.mkdir(basepath / dirname / subdirname)
 except Exception: pass 
 
 subsubdirname = 'simulation_results'
-
 try: Path.mkdir(basepath / dirname / subdirname / subsubdirname )
 except Exception: pass
 
@@ -562,23 +478,17 @@ with open(fpath / filename, mode = 'w', newline = '') as csv_file:
 message = '\nThe detailed files about shared energy have been saved in {}.'.format(fpath)
 print(message)  
 
-
-## Creating and storing figures
-
-dirname = 'Output'
-
-# Creating an /Output/Figures folder, if not already existing
-subdirname = 'Figures'
-
-try: Path.mkdir(basepath / dirname / subdirname)
-except Exception: pass
-
+#Store output results filename for later usage
+parametric_output_file = fpath / filename
 ##################################
 ########   PLOTS  ################
 ##################################
-
-# Different figures are created in case of fixed analysis for both the pv and the battery
-# or in case of at least one parametric analysis
+## Creating and storing figures
+dirname = 'Output'
+# Creating an /Output/Figures folder, if not already existing
+subdirname = 'Figures'
+try: Path.mkdir(basepath / dirname / subdirname)
+except Exception: pass
 
 # If there is at least one parametric analysis, figures showing the trend of the iss, isc and shared energy
 # depending on the sizes are created
@@ -677,10 +587,31 @@ if fixed_analysis_flag != 1:
 
 	filename = 'pv_{}_{}_battery_{}_{}_parametric_chart.png'.format(pv_size_range[0], pv_size_range[-1], battery_size_range[0], battery_size_range[-1])  
 	fig.savefig(fpath / filename) 
-	# plt.close(fig)
+	plt.show()
+	plt.clf() #clear
+	#Plot KPI Optimal Design
+	sns.set_theme() #use to reset955
+	sns.set_style("whitegrid")
+	sns.set_context("notebook", font_scale= 1.5)
+	plt.rc('figure',figsize=(24,16))
+	plt.rc('font', size=20)
+	plt.rc('lines', markersize = 18) 
+	output = pd.read_csv(parametric_output_file,
+					   sep = ";",
+					   decimal = ".",
+					   encoding='cp1252')
+	out_irr = output['IRR (%)']
+	out_pcr = output['PCR Yearly (%)']
 
-
-		
+	sns.scatterplot(data = output, 
+					x = 'PCR Yearly (%)',
+					y = 'IRR (%)',
+					hue = 'Battery size  (kWh)',
+					style = 'PV size  (kW)',
+					palette = 'flare',
+					legend = 'auto'
+					)
+	plt.title("KPI - Optimal Design")	
 
 
 # If the analysis is fixed-size on both the PV and the battery, detailed results (power fluxes)
@@ -756,13 +687,3 @@ print(message)
 
 print('\nEnd. Total time: {0:.2f} s.'.format(toc()))
 plt.show()
-
-
-
-
-
-
-
-
-
-
